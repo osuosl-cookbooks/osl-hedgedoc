ee79ef65a9711f04901c6ca7ffe56a5ef6474dca
Manatee <github@osuosl.org>
2026-06-05 16:56:52 +0000

    Automatic major-level version bump to v1.0.0 by Jenkins

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 This file is used to list changes made in each version of the osl-hedgedoc cookbook.
 
+1.0.0 (2026-06-05)
+------------------
+- Add osl_hedgedoc resource to deploy HedgeDoc via Docker Compose
+
 ## 0.1.0
 
 - Initial release
--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description       'Installs/Configures osl-hedgedoc'
 issues_url        'https://github.com/osuosl-cookbooks/osl-hedgedoc/issues'
 source_url        'https://github.com/osuosl-cookbooks/osl-hedgedoc'
 chef_version      '>= 18.0'
-version           '0.1.0'
+version           '1.0.0'
 
 depends           'osl-docker'
 depends           'osl-firewall'
